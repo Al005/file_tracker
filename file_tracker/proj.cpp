54fb8b7eb6ad36a57b12fa5d8e591eb4f2d2a803
Murad <tuhum_05@mail.ru>
2024-04-07 23:17:31 +0700

    Добавил файлы

---
+++ file_tracker/proj.cpp
@@ -0,0 +1,11 @@
+#include "proj.h"
+
+proj::proj(QWidget *parent)
+    : QWidget(parent)
+{
+}
+
+proj::~proj()
+{
+}
+
---
+++ file_tracker/proj.h
@@ -0,0 +1,14 @@
+#ifndef PROJ_H
+#define PROJ_H
+
+#include <QWidget>
+
+class proj : public QWidget
+{
+    Q_OBJECT
+
+public:
+    proj(QWidget *parent = nullptr);
+    ~proj();
+};
+#endif // PROJ_H
